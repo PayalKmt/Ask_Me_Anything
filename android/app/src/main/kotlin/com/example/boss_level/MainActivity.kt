4e38c7c24d5750bfdede0fffb8000f7a2bcdebdf
PayalKmt <kumawatpayal2005513@gmail.com>
2024-04-26 19:35:20 +0530

    Initial commit: Uploaded

---
+++ android/app/src/main/kotlin/com/example/boss_level/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.boss_level
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  return runApp(
+    const MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int? ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(4)+1;
+            print('I got clicked $ballNumber');
+          });
+        },
+        child: Image.asset('images/boll$ballNumber.png'),
+      ),
+    );
+  }
+}
